bf44517a2caa38de1a1e850fb4765d0563a30ea5
Akash Shaw <shawakash2003@gmail.com>
2024-01-06 10:40:10 +0530

    feat: seperating client and server components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Inter as FontSans } from "next/font/google"
 import { ThemeProvider } from "@/app/components/Client/theme-provider"
 import { ModeToggle } from './components/Client/ModeToggle'
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ['latin'] })
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 export const metadata: Metadata = {
   title: 'Repo Manager',
@@ -18,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body  className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -26,7 +33,6 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <ModeToggle />
-
           {children}
         </ThemeProvider>
       </body>
