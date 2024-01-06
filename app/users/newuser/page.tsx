e204129507766488302306945d20c562b6de1a31
Akash Shaw <shawakash2003@gmail.com>
2024-01-06 07:50:04 +0530

    feat: dockerizing the app

---
+++ app/users/newuser/page.tsx
@@ -0,0 +1,12 @@
+import AddToCart from '@/app/components/Client/AddToCart';
+import React from 'react';
+
+const NewUser = () => {
+  return (
+    <>
+        <AddToCart />
+    </>
+  );
+};
+
+export default NewUser;
